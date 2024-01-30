1222a3d23c76cca1d18dec776896d16c6da0bd0b
Florencia Scarabotti <florenciascarabotti@gmail.com>
2024-01-29 23:59:00 -0300

    eliminar familia. ok. Por ahora esta todo de USUARIO Y FAMILIA

--- sistema/eliminarFamilia.php
+++ sistema/eliminarFamilia.php
@@ -8,54 +8,38 @@
 
 	if(!empty($_POST)){
 
-		if( $_POST['id_usuario']==1){
-			header("location: listaUsuario.php");
-			mysqli_close($conn);
-			exit;
-		}
-
-		$id_usuario = $_POST['id_usuario'];
-		$query_check_rol = mysqli_query($conn, "SELECT id_rol FROM usuario WHERE id_usuario = $id_usuario");
-		$data_rol = mysqli_fetch_array($query_check_rol);
-		$id_rol_usuario = $data_rol['id_rol'];
-
-		if ($id_rol_usuario == 1) {
-			header("location: listaUsuario.php");
-			exit;
-		}
+		$id_tutor = $_POST['id_tutor'];
 		
-		$query_delete = mysqli_query($conn, "DELETE FROM usuario WHERE id_usuario = $id_usuario");
+		$query_delete = mysqli_query($conn, "DELETE FROM familia WHERE id_tutor = $id_tutor");
 		mysqli_close($conn);
 
 		if($query_delete){
-			header("location: listaUsuario.php");
+			header("location: listaFamilia.php");
 		}else{
 			echo "Error al eliminar";
 		}
 	}
 	
-	if(empty($_REQUEST['id']) || $_REQUEST['id'] ==1){
-		header("location: listaUsuario.php");
+	if(empty($_REQUEST['id'])){
+		header("location: listaFamilia.php");
 		mysqli_close($conn);
 	}else{
 		
-		$id_usuario = $_REQUEST['id'];
-		$query = mysqli_query($conn, "SELECT u.nombre, u.usuario, r.rol 
-											FROM usuario u 
-											INNER JOIN rol r 
-											on u.id_rol = r.id_rol 
-											WHERE u.id_usuario=$id_usuario" );
+		$id_tutor = $_REQUEST['id'];
+		$query = mysqli_query($conn, "SELECT f.dni_tutor, f.nombre_tutor, f.apellido_tutor 
+										FROM familia f 
+										WHERE f.id_tutor=$id_tutor" );
 		mysqli_close($conn);
 		$result = mysqli_num_rows($query);
 
 		if($result > 0){
 			while($data = mysqli_fetch_array($query)){
-				$nombre = $data['nombre'];
-				$usuario = $data['usuario']; 	
-				$rol = $data['rol'];
+				$dni_tutor = $data['dni_tutor'];
+				$nombre = $data['nombre_tutor'];
+				$apellido = $data['apellido_tutor']; 	
 			}
 		}else{
-			header("location: listaUsuario.php");
+			header("location: listaFamilia.php");
 		}
 	}
 ?>
@@ -65,7 +49,7 @@
 <head>
 	<meta charset="UTF-8">
 	<?php include "includes/scripts.php"; ?>
-	<title>Eliminar Fam ilia</title>
+	<title>Eliminar Familia</title>
 </head>
 <body>
 
@@ -74,13 +58,13 @@
 	<section id="container">
 		<div class="data_delete">
 			<h2>¿Estas seguro de eliminar el siguiente Usuario?</h2>
+			<p>DNI: <span><?php echo $dni_tutor; ?></span></p>
 			<p>Nombre: <span><?php echo $nombre; ?></span></p>
-			<p>Usuario: <span><?php echo $usuario; ?></span></p>
-			<p>Tipo de Usuario: <span><?php echo $rol; ?></span></p>
+			<p>Apellido: <span><?php echo $apellido; ?></span></p>
 
 			<form action="" method="post">
-				<input type="hidden" name="id_usuario" value="<?php echo $id_usuario; ?>">
-				<a class="btn_cancel" href="listaUsuario.php">Cancelar</a>
+				<input type="hidden" name="id_tutor" value="<?php echo $id_tutor; ?>">
+				<a class="btn_cancel" href="listaFamilia.php">Cancelar</a>
 				<input class="btn_ok" type="submit" value="Aceptar">
 			</form>
 		</div>
