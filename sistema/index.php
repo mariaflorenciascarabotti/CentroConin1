55da2c5648d7a33d8afafad27b62d315d2fa7b82
Florencia Scarabotti <florenciascarabotti@gmail.com>
2024-02-13 23:13:49 -0300

    agregue manual de usuario al inicio al inicio

--- sistema/index.php
+++ sistema/index.php
@@ -15,7 +15,18 @@
 	<section id="container">
 		
 		<h2 class="prueba">Bienvenido al sistema</h2>
-
+		<p class="fs-5">Por favor, tomese un momento para leer el 
+			<a href="../img/Manual de Usuario 2024 -Conin.pdf" target="_blank" rel="noopener noreferrer">
+				"Manual de Usuario"
+			</a>y entender su funcionamiento.
+		</p>
+		
+		<div class="manualUsuario">
+			<a href="../img/Manual de Usuario 2024 -Conin.pdf" target="_blank" rel="noopener noreferrer">
+				<img src="../img/manual_usuario.png" alt="Manual de Usuario" width="150">
+			</a>
+		</div>
+		
 	</section>
 
 	<?php include "includes/footer.php"; ?>
